b8df882f4433bcdcb03f78b3cacd21148a9c2fca
JacobDDoS <jacdimmitt@gmail.com>
2022-02-06 11:38:22 -0600

    Added What is it page

--- src/screens/WhatIsIt.js
+++ src/screens/WhatIsIt.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import {
+  StyleSheet,
   Text,
   View,
 } from 'react-native';
+import HomeButton from '../components/HomeButton';
 
-const WhatIsIt = () => {
+const WhatIsIt = ({ navigation }) => {
   return <View>
-    <Text>WHATISIT!!</Text>
+    <View style={{alignItems: 'center'}}>
+      <Text style={{fontSize: 40, fontWeight: "900", color: "rgb(77, 87, 95)", margin: 20}}>What is it?</Text>
+    </View>
+    <View>
+      <Text style={textStyles.bulletText}>- New Iteration of C</Text>
+    </View>
+    <View>
+      <Text style={textStyles.bulletText}>- Adds Object Oriented Programming</Text>
+    </View>
+    <View style={{alignItems: 'center', marginTop: 30}}>
+      <HomeButton onPressHandler={()=>navigation.navigate("Home")}/>
+    </View>
   </View>
 };
+
+const textStyles = StyleSheet.create({
+  bulletText: {
+    fontSize: 40,
+    marginLeft: 15,
+    fontWeight: "900",
+    color: "rgb(77, 87, 95)",
+    margin: 30,
+    marginLeft: 80
+  }
+})
 
 export default WhatIsIt;
